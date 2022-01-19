1008be81fc0294d9e3de64e83ad155283b71085c
TLC4_USER <emmanuelcudjoe16@yahoo.com>
2022-01-19 15:04:52 +0000

    #Comment Added a home component and refactored some code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule,  } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './add-user/add-user.component';
+import { HomeComponent } from './home/home.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { ViewUsersComponent } from './view-users/view-users.component';
 
 const routes: Routes = [
+  {
+    path: "", component: HomeComponent,
+  },
   {
     path: "user-details/:name", component: UserDetailsComponent,
   },
